e76aa8f5e53d04589a2d9a62dff0c8d51d0aec04
rgt320q <35804663+rgt320q@users.noreply.github.com>
2018-09-03 22:13:37 +0300

    Fix

--- HotelEntity/HotelEntity/HotelEntityWebMVCApp/Controllers/HomeController.cs
+++ HotelEntity/HotelEntity/HotelEntityWebMVCApp/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq.Dynamic;
 using System.Linq;
 using System.Web.Mvc;
 using HotelEntityWebMVCApp.Models;
+using System;
 
 namespace HotelEntityWebMVCApp.Controllers
 {
@@ -24,6 +25,44 @@ namespace HotelEntityWebMVCApp.Controllers
             
             return View(model);
 
+        }
+
+        public ActionResult List()
+        {
+            var model = db.BookingInformation
+                .Include(i => i.GuestInformation)
+                .Include(i => i.Payments);
+
+            return View(model.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BookingsAllModel bookingsAll)
+        {
+            if (ModelState.IsValid)
+
+            {
+                bookingsAll.BookingInformation.InsertDateTime = DateTime.Now;
+                bookingsAll.GuestInformation.InsertDateTime = DateTime.Now;                
+                bookingsAll.Payments.InsertDateTime = DateTime.Now;
+
+                bookingsAll.GuestInformation.GuestsequenceNo = 1;
+
+
+                db.BookingInformation.Add(bookingsAll.BookingInformation);
+                db.GuestInformation.Add(bookingsAll.GuestInformation);
+                db.Payments.Add(bookingsAll.Payments);
+
+                db.SaveChanges();
+            }
+
+            return Redirect("Index");
         }
     }
 }
