6de95dda6f44ffae6ea60e7c1b0cb4f0a4e0cc32
rgt320q <omercetin.msn@gmail.com>
2018-09-16 20:39:18 +0300

    Review fix

--- HotelEntity/HotelEntity/HotelEntityWebMVCApp/App_Start/RouteConfig.cs
+++ HotelEntity/HotelEntity/HotelEntityWebMVCApp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace HotelEntityWebMVCApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "BookingInformation", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
